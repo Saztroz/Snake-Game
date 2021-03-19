f42b07092795be812f9d39471dc19e8d3f21dc8a
Sain <hussein.rahal11@gmail.com>
2021-03-19 14:44:05 -0400

    added high score tracker

--- src/main.py
+++ src/main.py
@@ -25,6 +25,7 @@ game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
+    snake.move()
 
     #detect collision with food
     if snake.head.distance(food) < 15:
@@ -34,21 +35,15 @@ while game_is_on:
     
     #detect collision with wall
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.xcor() < -280:
-        game_is_on = False
-        scoreboard.game_over()
+        scoreboard.reset()
+        snake.reset()
+
 
     #detect collision with tail
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
-            game_is_on = False
-            scoreboard.game_over()
+            scoreboard.reset()
+            snake.reset()
     
-
-    snake.move()
-    
-
-
-
-
 
 screen.exitonclick()
